b749f9acc597cdc15bc637e9460e3d4ed59fb907
Zahid Hasan <zahidcse98@gmail.com>
2024-02-21 20:58:01 +0600

    clean repo

--- Accuweather/Utils/LocatorConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace unit5_final_task.Accuweather
-{
-    public class LocatorConstants
-    {
-        public const string PreciseTextLocator = "//*[text()='{0}']";
-        public const string PartialTextLocator = "//*[contains(text(),'{0}')]";
-    }
-}

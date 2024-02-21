09391093355800e8648a0dc24264c4a4ff8847d9
Zahid Hasan <zahidcse98@gmail.com>
2024-02-21 20:58:38 +0600

    upload project

---
+++ Accuweather/Hooks/Hooks.cs
@@ -0,0 +1,44 @@
+﻿using Aquality.Selenium.Browsers;
+using Aquality.Selenium.Core.Utilities;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace unit5_final_task.Accuweather.Hooks
+{
+    [Binding]
+    internal class Hooks
+    {
+        private readonly Browser browser = AqualityServices.Browser;
+        private static readonly JsonSettingsFile settings = new("config.json");
+
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            browser.Maximize();
+            browser.GoTo(settings.GetValue<string>("url"));
+            
+        }
+
+        [When(@"I go back to the Main Page")]
+        public void GoBackToTheMainPage()
+        {
+            browser.GoBack();
+            /*WebDriverWait wait = new WebDriverWait(AqualityServices.Browser.Driver, TimeSpan.FromSeconds(3));
+            wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.TagName("body")));*/
+        }
+
+
+
+        [AfterScenario]
+        public void TearDown()
+        {
+            browser.Quit();
+        }
+    }
+
+   
+}
---
+++ Accuweather/Pages/CityWeatherPage.cs
@@ -0,0 +1,20 @@
+﻿using Aquality.Selenium.Elements.Interfaces;
+using Aquality.Selenium.Forms;
+using OpenQA.Selenium;
+using System.Collections;
+
+namespace unit5_final_task.Accuweather.Pages
+{
+    internal class CityWeatherPage : Form
+    {
+        private const string pageName = "Weather Forecast | AccuWeather";
+        private ILabel cityName => ElementFactory.GetLabel(By.ClassName("header-loc"), "City Name");
+        public CityWeatherPage() : base(By.XPath(string.Format(LocatorConstants.PartialTextLocator, pageName)), "Accuweather HomePage") { }
+
+        public string GetCityName()
+        {
+            cityName.State.WaitForDisplayed();
+            return cityName.Text;
+        }
+    }
+}
